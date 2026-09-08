dd427dc4e383d8110134141a9c5a51071035d43a
Chris Law <165817311+ChrisLaw123@users.noreply.github.com>
2026-09-08 18:13:45 -0400

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<unordered_set<char>> square (9);
+        for (int i{}; i < board.size(); i++) {
+            unordered_set<char> row;
+            unordered_set<char> col;
+            
+            for (int j{}; j < board.size(); j++) {
+                if (row.contains(board[i][j])) {
+                    return false;
+                } else if (board[i][j] != '.'){
+                    row.insert(board[i][j]);
+                }
+
+                if (col.contains(board[j][i])) {
+                    return false;
+                } else if (board[j][i] != '.'){
+                    col.insert(board[j][i]);
+                }
+
+                int squarepos = (i/3) * 3 + (j/3);
+                if (square[squarepos].contains(board[i][j])) {
+                    return false;
+                } else if (board[i][j] != '.'){
+                    square[squarepos].insert(board[i][j]);
+                }
+
+            }
+        }
+
+        return true;
+    }
+};
